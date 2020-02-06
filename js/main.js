a6975a81f74f2fd8eb84adb08f2588863b674914
Елья <piragof@gmail.com>
2020-02-06 03:27:23 +0300

    начинает валидацию

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 'use strict';
 var CHECKIN_TIMES = ['12:00', '13:00', '14:00'];
 var CHECKOUT_TIMES = ['12:00', '13:00', '14:00'];
+var MOUSE_KEY = 0;
+// var MAINPIN_HEIGHT = 22;
 var Rooms = {
   MINIMUM: 1,
   MAXIMUM: 10
@@ -19,9 +21,11 @@ var template = document.querySelector('#pin').content.querySelector('.map__pin')
 var propertyTypes = ['palace', 'flat', 'house', 'bungalo'];
 var propertyFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var propertyPhotos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var mainPinAddressInput = document.querySelector('#address');
 var mainPin = document.querySelector('.map__pin--main');
 var fieldsets = document.querySelectorAll('fieldset');
 var mapFilters = document.querySelectorAll('select[class=map__filter]');
+var form = document.querySelector('.ad-form');
 
 var unfadeMap = function () {
   map.classList.remove('map--faded');
@@ -108,14 +112,15 @@ var changeMapState = function (object, newState) {
   }
 };
 
-changeMapState(fieldsets, false);
-changeMapState(mapFilters, false);
+changeMapState(fieldsets, true);
+changeMapState(mapFilters, true);
 
 var activateMap = function () {
   unfadeMap();
-  changeMapState(fieldsets, true);
-  changeMapState(mapFilters, true);
+  changeMapState(fieldsets, false);
+  changeMapState(mapFilters, false);
   drawPins(generateObjects(8));
+  form.classList.remove('ad-form--disabled');
   setAddress(mainPin, mainPinAddressInput);
 };
 
@@ -126,18 +131,13 @@ mainPin.addEventListener('keydown', function (evt) {
   }
 });
 
-var MOUSE_KEY = 0;
 mainPin.addEventListener('mousedown', function (evt) {
   if (evt.button === MOUSE_KEY) {
-  activateMap();
+    activateMap();
   }
 });
 
-var MAINPIN_HEIGHT = 22;
-var mainPin = document.querySelector('.map__pin--main');
-var mainPinAddressInput = document.querySelector('#address');
-
-function getCoords(elem) { // кроме IE8-
+var getCoords = function (elem) { // кроме IE8-
   var box = elem.getBoundingClientRect();
 
   return {
@@ -145,7 +145,7 @@ function getCoords(elem) { // кроме IE8-
     left: box.left + pageXOffset
   };
 
-}
+};
 
 var setAddress = function (object, objectInput) {
   if (map.classList.contains('map--faded')) {
@@ -159,6 +159,67 @@ var setAddress = function (object, objectInput) {
 
 setAddress(mainPin, mainPinAddressInput);
 
+/*  ----------------------------------------------- */
+var Preferences = {
+  MIN_TITLE_LENGTH: 30,
+  MAX_TITLE_LENGTH: 100
+};
+
+var ALL_PRICES = [0, 1000, 5000, 10000];
+var formTitle = form.querySelector('input[name=title]');
+var submitButton = form.querySelector('.ad-form__submit');
+var price = form.querySelector('input[name=price]');
+var houseType = form.querySelector('select[name=type]');
+// var timeIn = form.querySelector('select[name=timein]');
+// var timeOut = form.querySelector('input[name=timeout]');
+
+var getSelectedOption = function (object) {
+  var objectOptions = object.querySelectorAll('option');
+  for (var i = 0; i < objectOptions.length; i++) {
+    if (objectOptions[i].selected) {
+      var selectedOption = i;
+    }
+  }
+  return selectedOption;
+};
+
+var setOptions = function (evt) {
+  if (evt.target === houseType) {
+    price.value = ALL_PRICES[getSelectedOption(houseType)];
+    price.placeholder = price.value;
+  // } if (evt.target === timeIn) {
+  //   timeOut.value = CHECKOUT_TIMES[getSelectedOption(timeIn)];
+  //   timeOut.placeholder = timeOut.value;
+  }
+};
+
+form.addEventListener('change', setOptions);
+
+var colorChange = function (element) {
+  element.style = 'border-color: red; border-width: 5px;';
+};
+
+var validateTitle = function (title) {
+  if (title.length > Preferences.MAX_TITLE_LENGTH) {
+    formTitle.setCustomValidity('Максимальная длина — 100 символов');
+    colorChange(formTitle);
+    return false;
+  } else if (title.length < Preferences.MIN_TITLE_LENGTH) {
+    formTitle.setCustomValidity('Минимальная длина — 30 символов');
+    colorChange(formTitle);
+    return false;
+  }
+  return true;
+};
+
+var splitTitle = function () {
+  var titleForInput = formTitle.value.split('');
+  if (formTitle.value !== '') {
+    validateTitle(titleForInput);
+  }
+};
+
+submitButton.addEventListener('click', splitTitle);
 
 // var MAINPIN_HEIGHT = 22;
 // var mainPin = document.querySelector('.map__pin--main');
