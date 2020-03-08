26e3204157714e0dba546fed4e5697c92c22feee
elyaperagov <piragof@gmail.com>
2020-03-08 07:59:18 +0300

    небольшие правки по фильтрации

--- js/filter.js
+++ js/filter.js
@@ -16,23 +16,21 @@
     });
   };
 
+  var filterByHouseType = function (offer) {
+    var offersOfType = offer.filter(function (pin) {
+      if (houseTypeFilter.value === 'any') {
+        return pin.offer.type;
+      }
+      return houseTypeFilter.value === pin.offer.type;
+    });
+    window.card.removeOldCard();
+    window.data.removePins();
+    window.data.drawPins(offersOfType);
+  };
+
   var getHouseTypeFilter = function (offer) {
     houseTypeFilter.addEventListener('change', function () {
-      var offersOfType = offer.filter(function (pin) {
-        if (houseTypeFilter.value === 'flat') {
-          return pin.offer.type === 'flat';
-        } if (houseTypeFilter.value === 'bungalo') {
-          return pin.offer.type === 'bungalo';
-        } if (houseTypeFilter.value === 'house') {
-          return pin.offer.type === 'house';
-        } if (houseTypeFilter.value === 'palace') {
-          return pin.offer.type === 'palace';
-        }
-        return pin.offer.type;
-      });
-      window.data.removePins();
-      window.card.removeOldCard();
-      window.data.drawPins(offersOfType);
+      filterByHouseType(offer);
     });
   };
 
